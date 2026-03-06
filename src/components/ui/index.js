a1b17aa3da0fcc73460a5e7304cc92f6257ee3b1
Mingzhe Sun <smz_2009@qq.com>
2026-03-06 14:56:49 +0800

    Delete src directory

--- src/components/ui/index.js
+++
@@ -1,4 +0,0 @@
-export { default as InteractiveButton } from './InteractiveButton';
-export { default as SwipeableCard } from './SwipeableCard';
-export { LoadingSkeleton, SkeletonCard, ProgressBar, GeneratingLoader } from './LoadingSkeleton';
-export { PageTransition, Modal } from './PageTransition';

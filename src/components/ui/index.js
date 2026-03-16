9276c7b6e5b5f5b862c4f0956a26db0b3c1f9c43
volcano0813 <14790255928@163.com>
2026-03-16 19:35:23 +0800

    1935

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,3 +3,4 @@ export { default as SwipeableCard } from './SwipeableCard';
 export { LoadingSkeleton, SkeletonCard, ProgressBar, GeneratingLoader } from './LoadingSkeleton';
 export { PageTransition, Modal } from './PageTransition';
 export { default as LoadingTransition } from './LoadingTransition';
+export { default as StreamingAnalysisCard } from './StreamingAnalysisCard';
